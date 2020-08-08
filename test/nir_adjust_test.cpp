d5635ffa4364fc1080bc6ca3bfbe9c9301c39b60
michael081906 <mkam2@jh.edu>
2020-08-08 09:30:08 -0400

    add test

--- test/nir_adjust_test.cpp
+++ test/nir_adjust_test.cpp
@@ -1,30 +1,6 @@
 #include <ros/ros.h>
 #include <gtest/gtest.h>
-#include <mk_kernel/mkk.h>
-
-
-
-class nir_adjust_test : public ::testing::Test {
-  protected:
-    ros::NodeHandle n_;
-    ros::ServiceClient kernel_main_service_;
-    mk_kernel::mkk test_call;
-    // Setup
-    mkKernelTest()
-    { kernel_main_service_ = n_.serviceClient< mk_kernel::mkk > ("/kernel_main");
-  }
-};
-
-
-TEST_F(mkKernelTest, callCleanTest)
-{
-   test_call.request.task_id = 3;
-   if(kernel_main_service_.call(test_call))
-   {
-    EXPECT_EQ(test_call.response.error_, 0);
-  }
-}
-
+#include "nir_adjust.hpp"
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
